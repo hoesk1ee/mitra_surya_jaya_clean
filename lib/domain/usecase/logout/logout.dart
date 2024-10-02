bf83bf4a57dc14684680bcecc16f7bbd7d7119d5
hoeskiee <hoeskiee@gmail.com>
2024-10-02 18:23:44 +0700

    Implement logout usecase

---
+++ lib/domain/usecase/logout/logout.dart
@@ -0,0 +1,15 @@
+import 'package:mitra_surya_jaya_clean/data/repositories/authentication.dart';
+import 'package:mitra_surya_jaya_clean/domain/entities/result.dart';
+import 'package:mitra_surya_jaya_clean/domain/usecase/usecase.dart';
+
+class Logout implements UseCase<Result<void>, void> {
+  final Authentication _authentication;
+
+  Logout({required Authentication authentication})
+      : _authentication = authentication;
+
+  @override
+  Future<Result<void>> call(void _) {
+    return _authentication.logout();
+  }
+}

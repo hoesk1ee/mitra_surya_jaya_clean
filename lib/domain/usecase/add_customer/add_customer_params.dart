a0db5301b5f6ac217877a2cbe32e2df425c5ae7e
hoeskiee <hoeskiee@gmail.com>
2024-10-03 10:47:53 +0700

    implement add customer usecase

---
+++ lib/domain/usecase/add_customer/add_customer_params.dart
@@ -0,0 +1,10 @@
+class AddCustomerParams {
+  final String customerName;
+  final String customerPhone;
+  final String customerAddress;
+
+  const AddCustomerParams(
+      {required this.customerName,
+      required this.customerPhone,
+      required this.customerAddress});
+}
